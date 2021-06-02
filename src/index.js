ea68b65665be2b8cdc6e84bfbe54e348505e8f7d
Jesus <65581378+Jesuss27@users.noreply.github.com>
2021-06-01 21:53:50 -0700

    Pass the state of the board from Board to Square

--- src/index.js
+++ src/index.js
@@ -13,12 +13,8 @@ class Square extends React.Component {
 
     render() {
       return (
-        <button className="square" onClick={() => {
-            this.setState({
-                value:"X"
-            })
-        }}>
-          {this.state.value}
+        <button className="square" onClick={this.props.onClick}>
+          {this.props.value}
         </button>
       );
     }
@@ -32,8 +28,16 @@ class Square extends React.Component {
         };
     }
 
+    handleClick(i){
+        const squares= this.state.squares.slice();
+        squares[i] = "X" ;
+        this.setState({
+            squares:squares , 
+        })
+    }
+
     renderSquare(i) {
-      return <Square value={this.state.squares}  />;
+      return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
   
     render() {
